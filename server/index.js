ac62ab3c9f34bba8c0e9616be63a938b294d73a5
Jesse Swedlund <swedlundjesse@gmail.com>
2021-03-15 15:26:33 -0700

    added server

---
+++ server/index.js
@@ -0,0 +1,83 @@
+const path = require("path");
+const express = require("express");
+const morgan = require("morgan");
+const compression = require("compression");
+const PORT = process.env.PORT || 8080;
+const app = express();
+module.exports = app;
+
+/**
+ * In your development environment, you can keep all of your
+ * app's secret API keys in a file called `secrets.js`, in your project
+ * root. This file is included in the .gitignore - it will NOT be tracked
+ * or show up on Github. On your production server, you can add these
+ * keys as environment variables, so that they can still be read by the
+ * Node process on process.env
+ */
+
+const createApp = () => {
+  // logging middleware
+  app.use(morgan("dev"));
+
+  // body parsing middleware
+  app.use(express.json());
+  app.use(express.urlencoded({ extended: true }));
+
+  // compression middleware
+  app.use(compression());
+
+  // static file-serving middleware
+  app.use(express.static(path.join(__dirname, "..", "public")));
+
+  // any remaining requests with an extension (.js, .css, etc.) send 404
+  app.use((req, res, next) => {
+    if (path.extname(req.path).length) {
+      // let request = req.path.split('/')
+      // console.log(request[request.length - 1])
+      // if (request[request.length - 1] === 'bundle.js') {
+      //   res.sendFile(path.join(__dirname, '..', 'public/bundle.js'))
+      // } else if (request[request.length - 1] === 'style.css') {
+      //   res.sendFile(path.join(__dirname, '..', 'public/style.css'))
+      // } else if (request[request.length - 1] === 'mountaineering.jpg') {
+      //   res.sendFile(
+      //     path.join(__dirname, '..', 'public/images/mountaineering.jpg')
+      //   )
+      // } else {
+      const err = new Error("Not found");
+      err.status = 404;
+      //}
+    } else {
+      next();
+    }
+  });
+
+  // sends index.html
+  app.use("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "..", "public/index.html"));
+  });
+
+  // error handling endware
+  app.use((err, req, res, next) => {
+    console.error(err);
+    console.error(err.stack);
+    res.status(err.status || 500).send(err.message || "Internal server error.");
+  });
+};
+
+const startListening = () => {
+  // start listening (and create a 'server' object representing our server)
+  const server = app.listen(PORT, () =>
+    console.log(`Mixing it up on port ${PORT}`)
+  );
+};
+
+async function bootApp() {
+  await createApp();
+  await startListening();
+}
+
+if (require.main === module) {
+  bootApp();
+} else {
+  createApp();
+}
